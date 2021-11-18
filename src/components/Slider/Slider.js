8975aacb992b5167fc6327386b6192fd9a9ce9e0
shikuku <muasasteve26@gmali.com>
2021-11-18 14:24:14 +0300

    created slider

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,31 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Slider.css"
 import ArrowLeftOutlinedIcon from '@mui/icons-material/ArrowLeftOutlined';
 import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined';
 import {slides} from "../../data"
+
 const Slider=()=>{
-    const hadleClick=()=>{
-        alert("muasa")
+    const [slideIndex,setSlideIndex]=useState(0);
+    const hadleClick=(direction)=>{
+       if(direction==="left"){
+setSlideIndex(slideIndex>0?slideIndex-1:2)
+       }
+       else{
+           setSlideIndex(slideIndex<2?slideIndex+1:0)
+       }
+
     }
     return(
         <div className="wrapper">
-            <div className="left-arrow" onClick={hadleClick}>
+            <div className="left-arrow" onClick={()=>hadleClick("left")}>
                 <ArrowLeftOutlinedIcon />
             </div>
-            <div className="slide">
-                <div className="slide-image">
-                    <img src="/images/woman2.jpg" />
-                </div>
-<div className="slide-content">
-    <h2>HOLIDAY OFFERS</h2>
-    <p>Grab our December latest offers</p>
-    <button className="btn">Buy Now</button>
-</div>
-
-            </div>
+            {
+                slides.map(slide=>{
+                    return(
+                    <div className="slide" style={{backgroundColor:slide.bg,transform:`translatex(${slideIndex*-100}vw)`}}>
+                    <div className="slide-image">
+                        <img src={slide.img} />
+                    </div>
+    <div className="slide-content">
+        <h2>{slide.title}</h2>
+        <p>{slide.desc}</p>
+        <button className="btn">Buy Now</button>
+    </div>
+      </div>
+                    )
+                })
+            }
+  
 
            
-            <div className="right-arrow" onClick={hadleClick}>
+            <div className="right-arrow" onClick={()=>hadleClick("right")}>
                 <ArrowRightOutlinedIcon />
             </div>
         </div>
--- src/data.js
+++ src/data.js
@@ -1,22 +1,27 @@
+import { grey } from "@mui/material/colors";
+
 export const slides=[
     {
         id:1,
         img:"./images/woman2.jpg",
         title:"HOLLYDAY OFFERS",
-        desc:"Grab our December latest offers"
+        desc:"Grab our December latest offers",
+        bg: "rgb(223 ,223 ,183)"
     },
 
     {
         id:2,
         img:"./images/woman1.jpg",
         title:"CLEARANCE SALE",
-        desc:"Get latest products as we clear our stores"
+        desc:"Get latest products as we clear our stores",
+        bg:"#B4C6A6",
     },
     {
-        id:2,
+        id:3,
         img:"./images/woman3.jpg",
         title:"NEW ARRIVALS",
-        desc:"Get best quality products here"
+        desc:"Get best quality products here",
+        bg:"#5589A6",
     }
 ]
 
